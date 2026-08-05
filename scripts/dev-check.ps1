d7241dbcccde63d0435585927f4901898c563c93
Dong Quan Tran <divineasterisk@gmail.com>
2026-08-05 10:11:34 -0500

    Format all C++ sources in local dev check

--- scripts/dev-check.ps1
+++ scripts/dev-check.ps1
@@ -1,5 +1,4 @@
 param(
-  [string[]]$Files = @(),
   [string]$ClangFormat = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clang-format.exe",
   [string]$BuildDir = "build",
   [string]$Config = "Debug"
@@ -7,8 +6,11 @@ param(
 
 $ErrorActionPreference = "Stop"
 
-if ($Files.Count -gt 0) {
-  & $ClangFormat -i @Files
+$files = Get-ChildItem -Path src, tests, bench -Recurse -Include *.cpp, *.hpp, *.h |
+  Select-Object -ExpandProperty FullName
+
+if ($files.Count -gt 0) {
+  & $ClangFormat -i -style=file @files
 }
 
 cmake -S . -B $BuildDir
